857cfca730500f397aac479c2c3935ea1b170ab7
Vitalii Shevchuk <vitshev120@gmail.com>
2020-05-31 03:53:29 +0300

    Commited bugged animation, deleted unused dependencies

--- src/components/PostsSlider/PostsSlider.js
+++ src/components/PostsSlider/PostsSlider.js
@@ -2,27 +2,60 @@ import React, {useCallback, useEffect, useRef, useState} from 'react';
 import classes from "./PostsSlider.module.scss"
 import {Swipeable} from "react-touch";
 import Post from "./Post/Post";
+import { useParams, useLocation } from 'react-router-dom';
+
+function getElementOffset(element) {
+    const de = document.documentElement;
+    const box = element.getBoundingClientRect();
+    const top = box.top + window.pageYOffset - de.clientTop;
+    const left = box.left + window.pageXOffset - de.clientLeft;
+    return {top: top, left: left};
+}
 
 const PostsSlider = () => {
 
+    const isMountedRef = useRef(null);
+
     const [state, setState] = useState({
         activePostId: 0,
-        interval: null
+        interval: null,
+        posts: [
+            {title:"Неотложная помощь", text:"Если у Вас неотложная ситуация, Вам нужен совет ЛОР врача или рекомендации как поступить в той или иной ситуации.", isOpen: false, translationY: 0, hasToUpdate: false},
+            {title:"Заболевания носа", text:"Если у Вас неотложная ситуация, Вам нужен совет ЛОР врача или рекомендации как поступить в той или иной ситуации.", isOpen: false, translationY: 0, hasToUpdate: false},
+            {title:"Заболевания уха", text:"Если у Вас неотложная ситуация, Вам нужен совет ЛОР врача или рекомендации как поступить в той или иной ситуации.", isOpen: false, translationY: 0, hasToUpdate: false},
+            {title:"Заболевания горла", text:"Если у Вас неотложная ситуация, Вам нужен совет ЛОР врача или рекомендации как поступить в той или иной ситуации.", isOpen: false, translationY: 0, hasToUpdate: false}
+        ],
+        disableAnimations: false
     });
 
-    const posts = [
-        {title:"Неотложная помощь", text:"Если у Вас неотложная ситуация, Вам нужен совет ЛОР врача или рекомендации как поступить в той или иной ситуации."},
-        {title:"Заболевания носа", text:"Если у Вас неотложная ситуация, Вам нужен совет ЛОР врача или рекомендации как поступить в той или иной ситуации."},
-        {title:"Заболевания уха", text:"Если у Вас неотложная ситуация, Вам нужен совет ЛОР врача или рекомендации как поступить в той или иной ситуации."},
-        {title:"Заболевания горла", text:"Если у Вас неотложная ситуация, Вам нужен совет ЛОР врача или рекомендации как поступить в той или иной ситуации."}
 
-    ];
+    const openPost = useCallback((ref, id)=>{
+        if (state.posts[id].isOpen === false) {
+            const offset = getElementOffset(ref.current);
+            const translationY = offset.top;
 
-    const rendPosts = posts.map((element, number)=>(
-        <Post title={element.title} text={element.text} key={number}/>
-    ));
+            console.log(translationY);
 
-    const isMountedRef = useRef(null);
+            setState(prev => {
+                const posts = prev.posts.slice();
+                posts[id].translationY = translationY;
+                return {
+                    ...prev,
+                    posts
+                }
+            })
+        }else{
+            setState(prev => {
+                const posts = prev.posts.slice();
+                posts[id].translationY = 0;
+                posts[id].isOpen = false;
+                return {
+                    ...prev,
+                    posts
+                }
+            })
+        }
+    },[]);
 
     let setSlideInterval = useCallback(()=>{
         const interval = setInterval(() => {
@@ -36,9 +69,10 @@ const PostsSlider = () => {
             }else{
                 clearInterval(interval);
             }
-        }, 5000)
+        }, 5000);
         return interval;
     },[]);
+
 
     useEffect(() => {
         isMountedRef.current = true;
@@ -53,6 +87,32 @@ const PostsSlider = () => {
         return () => isMountedRef.current = false;
     }, [setSlideInterval]);
 
+
+
+    const { id } = useParams();
+    const location = useLocation().pathname;
+    useEffect(()=>{
+        if (location.includes("/post")){
+            if (id >= 0 && id <= 3){
+                setState(prev => {
+                    const posts = prev.posts.slice();
+                    posts[id].isOpen = true;
+                    posts[id].hasToUpdate = true;
+                    clearInterval(prev.interval);
+                    return{
+                        ...prev,
+                        posts,
+                        activePostId: id
+                    }
+                })
+            }
+        };
+    },[location]);
+
+    const rendPosts = state.posts.map((element, number)=>(
+        <Post title={element.title} text={element.text} key={number} isOpen={element.isOpen} id={number} openPost={openPost} translationY={element.translationY} hasToUpdate={element.hasToUpdate} disAnim={state.disableAnimations}/>
+    ));
+
     const postWrapCls = [classes.PostWrap];
 
     let dots = [0, 0, 0, 0];
@@ -62,14 +122,20 @@ const PostsSlider = () => {
             <div className={classes.Dot} key={number}/> :
             <div className={classes.Dot + " " + classes.open} key={number}/>
     ));
-    if (state.activePostId === 0) {
+
+    if (state.activePostId == 0) {
         postWrapCls.push(classes.first);
-    } else if (state.activePostId === 1) {
+    } else if (state.activePostId == 1) {
         postWrapCls.push(classes.second);
-    } else if (state.activePostId === 2) {
+    } else if (state.activePostId == 2) {
         postWrapCls.push(classes.third);
-    } else if (state.activePostId === 3) {
+    } else if (state.activePostId == 3) {
         postWrapCls.push(classes.fourth);
+    }
+
+
+    if (state.disableAnimations){
+        postWrapCls.push(classes.disAnim);
     }
 
     return (
--- src/components/hoc/Layout/Layout.js
+++ src/components/hoc/Layout/Layout.js
@@ -1,11 +1,7 @@
-import React, {useCallback} from 'react';
+import React from 'react';
 import classes from "./Layout.module.scss";
 import Header from "../../Header/Header";
 import Botter from "../../Botter/Botter";
-
-
-import ScrollTrigger from 'react-scroll-trigger';
-import TrackVisibility  from "react-on-screen";
 
 
 const Layout = ({children}) => {
