3afaeae50d5d0ac2ba6df0ba1da67a3d6b69cff9
ViktoriiaKharchenko <ivanovad86@gmail.com>
2020-06-05 12:06:44 +0300

    backend added, connection with mongodb

---
+++ server/routes/question-router.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const QuestionCtrl = require('../controllers/question-controller')
+
+const router = express.Router()
+
+router.post('/question', QuestionCtrl.createQuestion)
+router.put('/question/:id', QuestionCtrl.updateQuestion)
+router.delete('/question/:id', QuestionCtrl.deleteQuestion)
+router.get('/question/:id', QuestionCtrl.getQuestionById)
+router.get('/questions', QuestionCtrl.getQuestions)
+
+module.exports = router
