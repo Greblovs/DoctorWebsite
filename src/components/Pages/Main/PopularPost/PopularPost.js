31f2b520dcbbfbd4bd70f438e427c8a96f6ed029
vega <S.greblov@gmail.com>
2020-06-21 15:38:19 -0700

    created double posts

--- src/components/Pages/Main/PopularPost/PopularPost.js
+++ src/components/Pages/Main/PopularPost/PopularPost.js
@@ -15,22 +15,37 @@ const PopularPost = () => {
                 {second: "dsfdsf"},
                 {third: "dsfdf"}
             ]
-        }];
+        },
+            {
+                title: "Ангина",
+                // text: "Большинство пациентов безаплеляционно заявляют: 'Доктор у меня ангина'. Но так ли однозначна причина боли?",
+                text: "Большинство пациентов заявляют: 'Доктор у меня ангина'. Но так ли однозначна причина боли?",
+                paragraphs: [
+                    {first: "sadf"},
+                    {second: "dsfdsf"},
+                    {third: "dsfdf"}
+                ]
+            }];
+
 
     const rendPosts = posts.map((element, index) => {
         return (
-            <Post title={element.title} text={element.text} key={index} openPost={()=>{}}/>
+            <Post index = {index} title={element.title} text={element.text} key={index} openPost={()=>{}}/>
         )
     });
 
     return (
+        window.innerWidth > 660?
         <>
             <Heading text={"Популярные статьи"}/>
-
             {rendPosts}
-
             <button className={classes.MoreButton}>Больше статей</button>
-
+        </>
+        :
+        <>
+            <Heading text={"Популярные статьи"}/>
+            <Post index = {0} title={posts[0].title} text={posts[0].text}  openPost={()=>{}}></Post>
+            <button className={classes.MoreButton}>Больше статей</button>
         </>
     )
 };
