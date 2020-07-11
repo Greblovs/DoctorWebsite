09201acd33fde4e5ce4aead68298d9b39f565988
vega <S.greblov@gmail.com>
2020-07-11 00:08:13 -0700

    Admin Login Page

---
+++ src/components/Pages/AdminLogin/AdminPart/AdminPart.js
@@ -0,0 +1,12 @@
+import classes from "./AdminPart.module.scss"
+import React from "react";
+
+const AdminPart = () => {
+    return (
+        <>
+            <p style={{marginTop: "100px"}}>Сайт Админа</p>
+        </>
+    );
+};
+
+export default AdminPart;
