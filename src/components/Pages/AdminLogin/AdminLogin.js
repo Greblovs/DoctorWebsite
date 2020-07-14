45aec3982e4cdfbbf0b72c1b57643acbaaf7cfef
vega <S.greblov@gmail.com>
2020-07-14 03:43:58 -0700

    admin Part

--- src/components/Pages/AdminLogin/AdminLogin.js
+++ src/components/Pages/AdminLogin/AdminLogin.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import classes from "./AdminLogin.module.scss";
+import { useHistory } from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import AdminPart, {AdminPage} from "./AdminPart/AdminPart"
 
 const AdminLogin = () => {
+    const history = useHistory();
+    const nextPath = () => {
+        history.push("./admin/Pages");
+    }
+
+
     return (
         <>
-            <form className={classes.form}>
-                <span className={classes.Header}>Вход</span>
-                <input id = "login" placeholder={"Логин"} className = {classes.Login} type={"text"}/>
-                <input id = "password" placeholder={"Пароль"} className={classes.Password} type={"password"} />
-                <input  value={"Войти"} className={classes.Button} type = {"submit"} />
-            </form>
+            <Switch>
+                <Route path="/admin/Pages">
+                    <AdminPart/>
+                </Route>
+                <Route path="/admin">
+                    <form className={classes.form}>
+                        <span className={classes.Header}>Вход</span>
+                        <input id = "login" placeholder={"Логин"} className = {classes.Login} type={"text"}/>
+                        <input id = "password" placeholder={"Пароль"} className={classes.Password} type={"password"} />
+                        <input onClick={nextPath} value={"Войти"} className={classes.Button} type = {"submit"} />
+                    </form>
+                </Route>
+            </Switch>
         </>
     );
 };
--- src/components/Pages/AdminLogin/AdminPart/AdminPart.js
+++ src/components/Pages/AdminLogin/AdminPart/AdminPart.js
@@ -1,10 +1,37 @@
 import classes from "./AdminPart.module.scss"
 import React from "react";
+import {BrowserRouter, Route, Switch, NavLink} from "react-router-dom";
 
 const AdminPart = () => {
+    let ip = require("ip");
+    let string = "IP: " + ip.address()
+
+
     return (
         <>
-            <p style={{marginTop: "100px"}}>Сайт Админа</p>
+            <div className={classes.Menu}>
+                <div className={classes.ButtonWrapper}>
+                    <NavLink to="/admin/Pages/Questions" className={classes.Button}>Вопросы</NavLink>
+                    <NavLink to="/admin/Pages/Posts" className={classes.Button}>Посты</NavLink>
+                    <NavLink to="/admin/Pages/Contacts" className={classes.Button}>Контакты</NavLink>
+                </div>
+            </div>
+            <Switch>
+                <Route path="/admin/Pages/Questions" >
+
+                </Route>
+                <Route path="/admin/Pages/Posts" >
+
+                </Route>
+                <Route path = "/admin/Pages/Contacts">
+
+                </Route>
+                <Route path = "/admin/Pages">
+                    <p className={classes.Title}>Добро пожаловать Кот Вячеслав Федоровичь</p>
+                    <p className={classes.IP}>{string}</p>
+
+                </Route>
+            </Switch>
         </>
     );
 };
