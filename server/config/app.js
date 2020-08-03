f1794c3f2a460602c27351869b440ac07b519113
vega <S.greblov@gmail.com>
2020-08-02 18:36:48 -0700

    saved

--- server/config/app.js
+++ server/config/app.js
@@ -1,6 +1,6 @@
 module.exports = {
-    apiPort: 3002,
+    apiPort: 3001,
     mongoUri: 'mongodb://127.0.0.1:27017/EntSiteDatabase',
     jwtSecret: 'secKey',
-    mysqlpass:'RootSecPass1'
+    mysqlpass:'MakeMoreRobots'
 };
